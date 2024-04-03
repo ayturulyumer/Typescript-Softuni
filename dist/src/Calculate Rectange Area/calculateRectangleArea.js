404f2501a588f39a094700a013ae5242dbab3845
ajummer <makeudreamempty@gmail.com>
2024-04-03 12:37:29 +0300

    First Problem Solved

---
+++ dist/src/Calculate Rectange Area/calculateRectangleArea.js
@@ -0,0 +1,8 @@
+"use strict";
+function calculateRectangleArea(length, width) {
+    return length * width;
+}
+const a = 6;
+const b = 8;
+const area = calculateRectangleArea(a, b);
+console.log("The area of the rectangle is:", area);
---
+++ src/Calculate Rectange Area/calculateRectangleArea.ts
@@ -0,0 +1,9 @@
+function calculateRectangleArea(length: number, width: number): number {
+    return length * width;
+  }
+  
+  const a = 6;
+  const b = 8;
+  const area = calculateRectangleArea(a, b);
+  console.log("The area of the rectangle is:", area);
+  
